2287da0462970ad741c2c904ca20447db751646c
Kauã Joaquim <joaquiimkaua@gmail.com>
2026-04-18 17:43:07 -0300

    Add script to download and execute install.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+# 1. Baixar o script
+curl -fsSL https://raw.githubusercontent.com/ilyamiro/imperative-dots/master/install.sh -o install.sh
+
+# 2. Ler o conteúdo
+less install.sh
+
+# 3. (Opcional) tornar executável
+chmod +x install.sh
+
+# 4. Executar só se confiar
+./install.sh
